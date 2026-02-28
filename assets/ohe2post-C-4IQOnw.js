543fdb51a49a9b576137320897093908905f9368
greenhaha <greenhaha@users.noreply.github.com>
2026-02-28 09:56:43 +0000

    deploy: ce2a7f9cb454d1263fc3a2812817eb83491fae88 source-repo: kigermap/kigermap-frontend kigermap/kigermap-frontend@ce2a7f9cb454d1263fc3a2812817eb83491fae88

---
+++ assets/ohe2post-C-4IQOnw.js
@@ -0,0 +1,1 @@
+const t="https://cdn-frontend-static.kigermap.com/assets/ohe2post-BmCuOSWN.jpeg";export{t as P};
---
+++ assets/ohe2post-legacy-B1MvsWtA.js
@@ -0,0 +1,1 @@
+System.register([],function(t,e){"use strict";return{execute:function(){t("P","https://cdn-frontend-static.kigermap.com/assets/ohe2post-BmCuOSWN.jpeg")}}});
